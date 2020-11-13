f44401b2d0136633a5ee8672088b83eda5361e07
unknown <maximebarbotin1414@gmail.com>
2020-11-13 13:07:18 +0100

    premier commit

---
+++ src/components/HeaderNotConnected.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+        <div class="left_side_header">
+            <ul>
+                <li>Accueil</li>
+                <li>Posts</li>
+            </ul>
+        </div>
+        <div class="header_logo">
+            <img src="../src/images/icon-left-font-monochrome-black.png" alt="">
+        </div>
+        <div class="right_side_header">
+            <ul>
+                <li>Mon compte</li>
+                <li>Déconnexion</li>
+            </ul>
+        </div>
+    </div>
+    
+</template>
+
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
